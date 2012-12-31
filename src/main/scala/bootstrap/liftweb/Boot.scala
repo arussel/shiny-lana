e450211d2cd5e5e84203ce79b074deffbc17d3b4
Russel <alexandre@russel.fr>
2012-12-31 19:11:42 +0100

    Failure becomes 404

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,6 +1,7 @@
 package bootstrap.liftweb
 
 import net.liftweb.http._
+import net.liftweb.common.{Full, Empty}
 
 class Boot {
 
@@ -10,5 +11,16 @@ class Boot {
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
     LiftRules.htmlProperties.default.set((r: Req) => new Html5Properties(r.userAgent))
 
+    LiftRules.dispatch.append({
+      case Req("a"::Nil, _, _) => () => for {
+        x <- Empty ?~ "This is Wrong" ~> 500
+      } yield OkResponse()
+      case Req("b" :: Nil, _, _) => () => for {
+        x <- Empty ?~ "This is still wrong"
+      } yield OkResponse()
+      case Req("c" :: Nil, _, _) => () => for {
+        x <- Full(3)
+      } yield OkResponse()
+    })
   }
 }
